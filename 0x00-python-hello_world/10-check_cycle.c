91bbd0ba9abfb3e96ffae9b2e2e6c270a4c6fa39
a1ee7 <adixi1337@gmail.com>
2024-01-01 22:18:04 +0200

    Fix 3

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
+ * check_cycle - check if linked list contain cycle
+ * @list: linked list checked
  *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if list has cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *sl = list;
+	listint_t *fa = list;
 
 	if (!list)
 		return (0);
 
-	while (slow && fast && fast->next)
+	while (sl && fa && fa->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		sl = sl->next;
+		fa = fa->next->next;
+		if (sl == fa)
 			return (1);
 	}
 
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -10,8 +10,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
