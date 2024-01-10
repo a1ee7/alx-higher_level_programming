8b8704750b175ea8a57805ed69a62f509ba00650
a1ee7 <adixi1337@gmail.com>
2024-01-10 23:27:42 +0200

    update 4

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[num**2 for num in row] for row in matrix]
+    return list(map(lambda e: replace if e == search else e, my_list))
+	
+	
